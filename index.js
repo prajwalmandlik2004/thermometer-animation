75aae34bc8382e5b89e18194aa9a86380c53c2cd
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2022-09-08 23:20:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const tempLoad = () => {
+    let temp = document.getElementById('temp');
+    temp.innerHTML = "&#xf2cb";
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2ca";
+        temp.style.color = "orange";
+    }, 1000)
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c9";
+        temp.style.color = "lawngreen";
+    }, 2000)
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c8";
+        temp.style.color = "yellow";
+    }, 3000)
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c7";
+        temp.style.color = "red";
+    }, 4000)
+}
+tempLoad();
+setInterval(tempLoad, 5000);
